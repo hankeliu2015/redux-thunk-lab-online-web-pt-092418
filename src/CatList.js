b7b5342d99f7ea3e666d5f749c81ab5d2949477e
github username <github email address>
2019-07-29 21:45:48 -0400

    able to fetch a simgle story

--- src/CatList.js
+++ src/CatList.js
@@ -19,7 +19,13 @@ class CatList extends Component {
   render () {
     // const list = this.props.catPics.map((cat, index) => <li  key={index}> {cat.title} </li> )
     return (
-      <div>{this.props.catPics.title}</div>
+      <div>
+        <li>Story ID: {this.props.catPics.id}</li>
+        <li>Title: {this.props.catPics.title}</li>
+        <li>URL: {this.props.catPics.url}</li>
+        <li>Score: {this.props.catPics.score}</li>
+
+      </div>
     )
   }
 }
