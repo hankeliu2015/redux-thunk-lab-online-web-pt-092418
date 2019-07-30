c1749122db0177812dba48d46371c8148aa9adc3
github username <github email address>
2019-07-29 21:24:13 -0400

    change the fetch uri to hacknews. Change payload from cats.image to cats. Change iternation in presentation component to li.

--- src/CatList.js
+++ src/CatList.js
@@ -1,16 +1,3 @@
-import React, { Component } from 'react'
-
-class CatList extends Component {
-  render () {
-    const list = this.props.catPics.map((cat, index) => <img  key={index} src={cat.url} height="20%" width="20%" alt="catImage" /> )
-    return (
-      <div>{list}</div>
-    )
-  }
-}
-export default CatList
-
-// //Hacker news testing: 
 // import React, { Component } from 'react'
 //
 // class CatList extends Component {
@@ -22,3 +9,16 @@ export default CatList
 //   }
 // }
 // export default CatList
+
+//Hacker news testing:
+import React, { Component } from 'react'
+
+class CatList extends Component {
+  render () {
+    const list = this.props.catPics.map((cat, index) => <li  key={index}> {cat.title} </li> )
+    return (
+      <div>{list}</div>
+    )
+  }
+}
+export default CatList
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,18 +1,5 @@
 // import fetch from 'isomorphic-fetch'
 
-export function fetchCats() {
-  return (dispatch) => {
-    dispatch({type: 'LOADING_CATS'});
-    return (
-      fetch('http://localhost:4000/db')
-      .then(response => response.json())
-      .then(cats => dispatch({type: 'FETCH_CATS',payload: cats.images}))
-    )
-  }
-}
-
-// // hacker news fetch testing:
-
 // export function fetchCats() {
 //   return (dispatch) => {
 //     dispatch({type: 'LOADING_CATS'});
@@ -23,3 +10,16 @@ export function fetchCats() {
 //     )
 //   }
 // }
+
+// // hacker news fetch testing:
+
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_CATS'});
+    return (
+      fetch('https://hacker-news.firebaseio.com/v0/item/8265435.json?print=pretty')
+      .then(response => response.json())
+      .then(cats => dispatch({type: 'FETCH_CATS',payload: cats}))
+    )
+  }
+}
